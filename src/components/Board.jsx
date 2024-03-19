ad3542ed80acd5a14e9286723fda639ab4006a03
shubham-mishra-2003 <shubhammishra3070@gmail.com>
2024-03-19 21:01:12 +0530

    Final Deployment

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,28 +1,41 @@
 import { useState, useEffect } from "react";
 import PreviousScore from "./PreviousScore";
+import toast from "react-hot-toast";
 
 const Board = () => {
   const [score, setScore] = useState(0);
   const [buttonText, setButtonText] = useState("Start");
-  const [intervalId, setIntervalId] = useState(null); // State to hold interval id
+  const [intervalId, setIntervalId] = useState(null);
+  const [timeLeft, setTimeLeft] = useState(60);
 
-  useEffect(() => {
-    updateScore();
-  }, [score]);
+  useEffect(
+    () => {
+      if (timeLeft === 0) {
+        clearInterval(intervalId);
+        saveScoreAndRefresh();
+      }
+    },
+    [timeLeft, intervalId]
+  );
+
+  useEffect(
+    () => {
+      updateScore();
+    },
+    [score]
+  );
 
   const createCircle = () => {
     const gameContainer = document.getElementById("game-container");
     const circle = document.createElement("div");
     circle.classList.add("circle");
-    circle.style.left =
-      Math.random() * (gameContainer.offsetWidth - 50) + "px";
-    circle.style.top =
-      Math.random() * (gameContainer.offsetHeight - 50) + "px";
+    circle.style.left = Math.random() * (gameContainer.offsetWidth - 50) + "px";
+    circle.style.top = Math.random() * (gameContainer.offsetHeight - 50) + "px";
 
     gameContainer.appendChild(circle);
 
     circle.addEventListener("click", () => {
-      setScore((prevScore) => prevScore + 1);
+      setScore(prevScore => prevScore + 1);
       gameContainer.removeChild(circle);
     });
 
@@ -39,41 +52,75 @@ const Board = () => {
   const handleButtonClick = () => {
     if (buttonText === "Start") {
       const id = setInterval(createCircle, 500);
-      setIntervalId(id); // Store interval id in state
-      setButtonText("Pause");
-    } else if (buttonText === "Pause") {
-      clearInterval(intervalId); // Clear interval when paused
+      setIntervalId(id);
       setButtonText("Save");
+      startTimer();
     } else if (buttonText === "Save") {
-      saveScore();
+      const previousScores =
+        JSON.parse(localStorage.getItem("previousScores")) || [];
+      previousScores.push(score);
+      localStorage.setItem("previousScores", JSON.stringify(previousScores));
+      setButtonText("Start");
+      setScore(0);
+      setTimeLeft(60);
+      clearInterval(intervalId);
+      toast.success("Score saved");
+      setTimeout(() => {
+        window.location.reload();
+      }, 1100);
     }
   };
 
-  const saveScore = () => {
-    const previousScores = JSON.parse(localStorage.getItem("previousScores")) || [];
+  const startTimer = () => {
+    const timerId = setInterval(() => {
+      setTimeLeft(prevTime => prevTime - 1);
+    }, 1000);
+    setIntervalId(timerId);
+  };
+
+  const saveScoreAndRefresh = () => {
+    const previousScores =
+      JSON.parse(localStorage.getItem("previousScores")) || [];
     previousScores.push(score);
     localStorage.setItem("previousScores", JSON.stringify(previousScores));
     setButtonText("Start");
     setScore(0);
+    setTimeLeft(60);
+    toast.error("Oops!! time out");
+    setTimeout(() => {
+      toast.success("Score saved");
+    }, 1100);
+    setTimeout(() => {
+      window.location.reload();
+    }, 2100);
   };
 
   return (
-    <div className="flex flex-col items-center justify-between mt-28 pr-10 pl-10 md:pr-16 md:pl-16 gap-3 pb-3">
+    <div className="flex flex-col items-center justify-between mt-28 pr-10 pl-10 md:pr-16 md:pl-16 pb-3">
+      <span className="md:text-xl text-lg text-slate-400">
+        Time Left : {Math.floor(timeLeft / 60)}:
+        {(timeLeft % 60).toLocaleString("en-US", {
+          minimumIntegerDigits: 2,
+          useGrouping: false
+        })}
+      </span>
       <div
-        className="relative p-5 bg-gray-700 rounded-xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100 h-[60vh] w-full"
+        className="relative p-5 bg-gray-700 rounded-b-none rounded-xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100 h-[60vh] w-full"
         id="game-container"
       />
-      <div className="w-full flex flex-col md:flex-row justify-center items-center md:gap-6 gap-2">
+      <span
+        className="md:text-2xl text-center text-lg text-slate-400 bg-gray-700 rounded-xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100 w-full rounded-t-none border-t-0"
+        id="score"
+      />
+
+      <div className="w-full flex justify-between items-center flex-col md:flex-row md:gap-6 mt-2 gap-2">
         <button
           className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-4 w-full md:w-80 rounded-3xl scale-90 transition-all text-xl active:scale-100 font-bold font-serif"
           onClick={handleButtonClick}
         >
           {buttonText}
         </button>
-        <div className="flex justify-center items-center gap-5">
-          <p className="md:text-3xl text-lg text-slate-400" id="score" />
-          <PreviousScore />
-        </div>
+        <PreviousScore />
       </div>
     </div>
   );
--- src/components/PreviousScore.jsx
+++ src/components/PreviousScore.jsx
@@ -3,7 +3,8 @@ import CloseIcon from "@mui/icons-material/Close";
 
 const PreviousScore = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const previousScores = JSON.parse(localStorage.getItem("previousScores")) || [];
+  const previousScores =
+    JSON.parse(localStorage.getItem("previousScores")) || [];
 
   const openModal = () => {
     setIsOpen(true);
@@ -13,22 +14,28 @@ const PreviousScore = () => {
     setIsOpen(false);
   };
 
-  const getNumberSuffix = (index) => {
+  const getNumberSuffix = index => {
     if (index === 1) return "st";
     else if (index === 2) return "nd";
     else if (index === 3) return "rd";
     else return "th";
   };
 
+  const handleDeleteScores = () => {
+    localStorage.removeItem("previousScores");
+    alert("All saved scores will be deleted!");
+    closeModal();
+  };
+
   return (
-    <div>
+    <div className="md:w-fit w-full">
       <button
         onClick={openModal}
-        className="p-2 rounded-lg bg-gradient-to-r from-sky-500 to-indigo-500 text-lg font-bold font-serif"
+        className="bg-gradient-to-r from-sky-500 to-indigo-500 p-4 w-full md:w-80 rounded-3xl scale-90 transition-all text-xl active:scale-100 font-bold font-serif"
       >
         Previous Scores
       </button>
-      {isOpen && (
+      {isOpen &&
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[65vw] md:w-[30vw] p-0">
           <div className="bg-gray-200 flex justify-between flex-row-reverse gap-4 p-3 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-gray-100">
             <span
@@ -38,20 +45,30 @@ const PreviousScore = () => {
               <CloseIcon />
             </span>
             <div>
-              <h1 className="md:text-3xl text-xl font-bold font-serif">Previous Scores :</h1>
-              {previousScores.length > 0 ? (
-                <ul className="list-disc pl-4">
-                  {previousScores.map((score, index) => (
-                    <li key={index} className="mt-2 text-xl">{index + 1}{getNumberSuffix(index + 1)} - {score}</li>
-                  ))}
-                </ul>
-              ) : (
-                <p className="mt-2">No scores saved yet</p>
-              )}
+              <h1 className="md:text-3xl text-xl font-bold font-serif">
+                Previous Scores :
+              </h1>
+              {previousScores.length > 0
+                ? <ul className="list-disc pl-6 max-h-60 overflow-auto w-full">
+                    {previousScores.map((score, index) =>
+                      <li key={index} className="mt-2 text-xl">
+                        {index + 1}
+                        {getNumberSuffix(index + 1)} - {score}
+                      </li>
+                    )}
+                  </ul>
+                : <p className="mt-2">No scores saved yet</p>}
             </div>
           </div>
-        </div>
-      )}
+          {previousScores.length > 0
+            ? <button
+                onClick={handleDeleteScores}
+                className="w-full bg-gray-200 rounded-md mt-1 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border p-1 border-gray-100"
+              >
+                Delete
+              </button>
+            : null}
+        </div>}
     </div>
   );
 };
