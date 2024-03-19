59730d96481f2c46d606b12ce5ee81c416b32111
shubham-mishra-2003 <shubhammishra3070@gmail.com>
2024-03-19 15:12:07 +0530

    Complete design

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen overflow-hidden">
+    <div className="h-full overflow-hidden">
       <Header />
       <Board />
     </div>
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,21 @@
 const Board = () => {
   return (
-    <div className="p-5 bg-gray-700 rounded-xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100">
-      <div id="" className="rounded-full h-10 w-10 bg-amber-300 border-2 border-slate-500" />
+    <div className="flex flex-col items-center justify-between mt-28 pr-10 pl-10 md:pr-16 md:pl-16 gap-3">
+      <div
+        className="relative p-5 bg-gray-700 rounded-xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100 h-[60vh] w-full"
+        id="game-container"
+      >
+        <div className="circle" />
+      </div>
+      <div className="w-full flex flex-col md:flex-row justify-center items-center md:gap-6 gap-2">
+        <button
+          className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-4 w-full md:w-80 rounded-3xl scale-90 transition-all text-xl active:scale-100"
+          id="start"
+        >
+          Start
+        </button>
+        <p className="text-2xl text-slate-400">Score : 0</p>
+      </div>
     </div>
   );
 };
