37473369c7cbb702959576d9546ff977a82f211d
shubham-mishra-2003 <shubhammishra3070@gmail.com>
2024-03-19 14:25:21 +0530

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import Board from "./components/Board";
 import Header from "./components/Header";
-import Score from "./components/Score";
 
 function App() {
   return (
     <div className="flex justify-center items-center min-h-screen overflow-hidden">
       <Header />
       <Board />
-      <Score />
     </div>
   );
 }
